695d7f29d5fdce861ee71d3b4ec0f2b969751222
Alexseyka07 <117672070+Alexseyka07@users.noreply.github.com>
2022-12-05 20:35:56 +0300

    Update
    Update(0)

--- Alexseyka07TestConsoleApp_M2/Program.cs
+++ Alexseyka07TestConsoleApp_M2/Program.cs
@@ -10,6 +10,8 @@ namespace Alexseyka07TestConsoleApp_M2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Vlad!");
+            Console.ReadLine();
         }
     }
 }
